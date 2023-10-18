447eea63f9ec0ff22294c2de4546b0cea2277fe2
vineet kumar <145231158+vineet0o@users.noreply.github.com>
2023-10-18 19:53:56 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int savings;
+    cin>>savings;
+    if(savings>5000){
+        cout<<"chandan\n";
+    }else{
+        cout<<"pradeep\n";
+    }
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the 1st no\n";
+    cin>>a;
+    cout<<"enter the 2nd no\n";
+    cin>>b;
+    int sum=a+b;
+    cout<<sum;
+    return 0;
+
+
+}
